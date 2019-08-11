7d724b37e9f63a475fe12346e9938943994bf14b
Jim Madge <jmmadge@gmail.com>
2019-08-11 13:28:13 +0100

    Add better solution for problem 8 with comment

--- 08.hs
+++ 08.hs
@@ -9,5 +9,12 @@ removeAllLeading a (x:xs)
 compress [] = []
 compress (x:xs) = x:(removeAllLeading x xs)
 
+-- A more consise solution from the wiki.
+-- Here, the @ symbols lets us name the remainer of the list 'ys' with head 'y'
+-- compress (x:ys@(y:_))
+    -- | x == y    = compress ys
+    -- | otherwise = x : compress ys
+-- compress ys = ys
+
 main = do
     print $ compress "aaaabccaadeeee"
