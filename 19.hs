df0337672893b40b2563c94c314aaed979bd2594
Jim Madge <jmmadge@gmail.com>
2019-08-17 20:14:54 +0100

    Add better solution for problem 19

--- 19.hs
+++ 19.hs
@@ -13,15 +13,24 @@
 --         where (first, last) = splitAt n xs
 
 -- Works for positive and negative, but a bit messy
+-- rotate [] _ = []
+-- rotate xs n
+--     | abs n > length xs = error "n greater than length of list"
+--     | n == 0 = xs
+--     | abs n == length xs = xs
+--     | n > 0 = last ++ first
+--     | n < 0 = last' ++ first'
+--         where (first, last) = splitAt n xs
+--               (first', last') = splitAt (length xs + n) xs
+
+-- Recursive solution inspired by the wiki solution
+-- This is better because it allows to the number of rotations to be greater
+-- than the length of the list.
 rotate [] _ = []
-rotate xs n
-    | abs n > length xs = error "n greater than length of list"
-    | n == 0 = xs
-    | abs n == length xs = xs
-    | n > 0 = last ++ first
-    | n < 0 = last' ++ first'
-        where (first, last) = splitAt n xs
-              (first', last') = splitAt (length xs + n) xs
+rotate xs 0 = xs
+rotate (x:xs) n
+    | n >= 0 = rotate (xs ++ [x]) (n-1)
+    | n < 0 = rotate xs (length xs + n)
 
 main = do
     print $ rotate ['a','b','c','d','e','f','g','h'] 3
