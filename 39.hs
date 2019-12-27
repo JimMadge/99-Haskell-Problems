6132af895df7e4946e0955572ec8ec460b4768c0
Jim Madge <jmmadge@gmail.com>
2019-12-27 12:11:43 +0000

    Improve problem 39 solution

--- 39.hs
+++ 39.hs
@@ -9,7 +9,9 @@ isPrime n = all ((/=0) . mod n) $ takeWhile (<= m) candidates
         where candidates = (2:3:[x + i | x <- [6,12..], i <- [-1,1]])
               m = floor . sqrt $ fromIntegral n
 
-primesR low high = [x | x <- [low..high], isPrime x]
+primesR low high
+    | even low = [x | x <- [low+1,low+3..high], isPrime x]
+    | otherwise = [x | x <- [low,low+2..high], isPrime x]
 
 main = do
     print $ primesR 10 20
